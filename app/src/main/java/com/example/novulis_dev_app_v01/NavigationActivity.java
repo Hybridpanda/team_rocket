24cc175e955b7eedb94db84c456ae209c15add00
JayWilling <jwilling123@gmail.com>
2021-09-21 14:53:37 +1000

    Fragments added but not setup

--- app/src/main/java/com/example/novulis_dev_app_v01/NavigationActivity.java
+++ app/src/main/java/com/example/novulis_dev_app_v01/NavigationActivity.java
@@ -1,14 +1,35 @@
 package com.example.novulis_dev_app_v01;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarItemView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class NavigationActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    NavigationBarItemView navigationBarItemView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch(id) {
+                    // check
+                }
+                return true;
+            }
+        });
     }
 }
