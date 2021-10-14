c9c500397be503b402ebd0a4a464d90e71b3adad
PatrickCasey <pcasey10101@gmail.com>
2021-10-15 04:26:11 +1100

    got a null pointer to eragon

--- app/src/main/java/com/example/novulis_dev_app_v01/activities/PercyJacksonActivity.java
+++ app/src/main/java/com/example/novulis_dev_app_v01/activities/PercyJacksonActivity.java
@@ -2,7 +2,9 @@ package com.example.novulis_dev_app_v01.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.example.novulis_dev_app_v01.R;
 
@@ -12,5 +14,16 @@ public class PercyJacksonActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_percy_jackson);
+
+        ImageView nextBtn = findViewById(R.id.next);
+        nextBtn.setOnClickListener(view -> {
+            Intent i = new Intent(this, InProgressActivity.class);
+            startActivity(i);
+        });
+        ImageView backBtn = findViewById(R.id.erBack);
+        backBtn.setOnClickListener(view -> {
+            Intent i = new Intent(this, EragonActivity.class);
+            startActivity(i);
+        });
     }
 }
