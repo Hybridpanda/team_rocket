4b04d80c698bca39c724d81dd2d9dfedb417cc35
PatrickCasey <pcasey10101@gmail.com>
2021-10-23 19:26:19 +1100

    animation in book selection

--- app/src/main/java/com/example/novulis_dev_app_v01/fragments/ClubSelectionFragment.java
+++ app/src/main/java/com/example/novulis_dev_app_v01/fragments/ClubSelectionFragment.java
@@ -1,14 +1,16 @@
 package com.example.novulis_dev_app_v01.fragments;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import com.example.novulis_dev_app_v01.R;
@@ -41,9 +43,11 @@ public class ClubSelectionFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_club_selection, container, false);
-
+        ObjectAnimator translateAnimation = ObjectAnimator.ofFloat(teamRocketBtn, view.TRANSLATION_Y, 0);
+        translateAnimation.setRepeatCount(1);
+        translateAnimation.setRepeatMode( ValueAnimator.REVERSE);
+        Log.d("done", "done");
         teamRocketBtn = view.findViewById(R.id.teamRocketBtn);
 
         teamRocketBtn.setOnClickListener(new View.OnClickListener() {
